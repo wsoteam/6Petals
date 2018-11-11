e93a5c65f79b32fea181f3edefb7fea412af691d
Nikolay <ny@wsoteam.com>
2018-11-11 22:09:40 +0300

    fix diary activity

--- app/src/main/java/com/wsoteam/diet/MainScreen/MainActivity.java
+++ app/src/main/java/com/wsoteam/diet/MainScreen/MainActivity.java
@@ -162,7 +162,7 @@ public class MainActivity extends AppCompatActivity
         cvDiary.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ActivityDescription.class);
+                Intent intent = new Intent(MainActivity.this, ActivityEmpty.class);
                 startActivity(intent);
             }
         });
