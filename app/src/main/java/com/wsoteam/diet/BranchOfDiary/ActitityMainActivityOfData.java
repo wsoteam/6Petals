5c0d4fda712a314213cad8e59dac8b8208ad52c5
wkazak <wk@wsoteam.com>
2018-11-12 19:38:53 +0300

    draw item for diary list

--- app/src/main/java/com/wsoteam/diet/BranchOfDiary/ActitityMainActivityOfData.java
+++ app/src/main/java/com/wsoteam/diet/BranchOfDiary/ActitityMainActivityOfData.java
@@ -2,15 +2,30 @@ package com.wsoteam.diet.BranchOfDiary;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.wsoteam.diet.BranchOfMonoDiets.ActivityMonoDiet;
+import com.wsoteam.diet.BranchOfMonoDiets.ActivityViewerOfBodyItem;
+import com.wsoteam.diet.Config;
+import com.wsoteam.diet.POJOS.POJO;
 import com.wsoteam.diet.R;
+
+import java.util.ArrayList;
 
 public class ActitityMainActivityOfData extends AppCompatActivity {
     private FloatingActionButton fabAddData;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,6 +33,9 @@ public class ActitityMainActivityOfData extends AppCompatActivity {
         setContentView(R.layout.activity_main_activity_of_data_diary);
 
         fabAddData = findViewById(R.id.fabAddDataListOfDiary);
+        recyclerView = findViewById(R.id.rvListOfDiary);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new ItemAdapter());
 
 
         fabAddData.setOnClickListener(new View.OnClickListener() {
@@ -28,4 +46,41 @@ public class ActitityMainActivityOfData extends AppCompatActivity {
             }
         });
     }
+
+
+    private class ItemHolder extends RecyclerView.ViewHolder {
+        TextView tvItem;
+        ImageView ivItem;
+
+        public ItemHolder(LayoutInflater layoutInflater, ViewGroup viewGroup) {
+            super(layoutInflater.inflate(R.layout.item_diary_data, viewGroup, false));
+        }
+
+        public void bind(POJO title) {
+        }
+
+    }
+
+    private class ItemAdapter extends RecyclerView.Adapter<ItemHolder> {
+
+
+        @NonNull
+        @Override
+        public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(ActitityMainActivityOfData.this);
+            return new ItemHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
+    }
+
+
 }
