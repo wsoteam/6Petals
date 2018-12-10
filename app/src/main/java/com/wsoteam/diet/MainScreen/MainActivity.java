f10a1bae616b792e391d4f887dbec118be6c3486
wkazak <wk@wsoteam.com>
2018-12-10 20:24:41 +0300

    new design main menu

--- app/src/main/java/com/wsoteam/diet/MainScreen/MainActivity.java
+++ app/src/main/java/com/wsoteam/diet/MainScreen/MainActivity.java
@@ -1,9 +1,7 @@
 package com.wsoteam.diet.MainScreen;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.content.ComponentName;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.graphics.drawable.Animatable2;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
@@ -15,45 +13,48 @@ import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.NotificationCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.reward.RewardedVideoAd;
-import com.wsoteam.diet.BranchOfDiary.ActivityListOfDiary;
 import com.wsoteam.diet.BranchOfDescription.ActivityDescription;
 import com.wsoteam.diet.BranchOfNews.ActivityListOfNews;
 import com.wsoteam.diet.BranchOfNotifications.ActivityListOfNotifications;
-import com.wsoteam.diet.BranchOfRecipes.ActivityGroupsOfRecipes;
 import com.wsoteam.diet.Config;
-import com.wsoteam.diet.Manifest;
-import com.wsoteam.diet.OtherActivity.ActivityEmpty;
-import com.wsoteam.diet.BranchOfCalculating.ActivityListOfCalculating;
-import com.wsoteam.diet.BranchOfMonoDiets.ActivityMonoDiet;
 import com.wsoteam.diet.R;
 import com.yandex.metrica.YandexMetrica;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    private CardView cvMonoDiets, cvCalculating, cvDiary, cvRecipes, cvAnalyzator;
-    private ImageView ivDiets, ivCalculating, ivDiary, ivRecipes, ivAnalyzator;
     private AnimatedVectorDrawable animatedVectorDrawable;
     private RewardedVideoAd mRewardedVideoAd;
-    Toolbar toolbar;
+    private Toolbar toolbar;
+    private RecyclerView rvMainList;
+
+    private Integer [] urlsOfImages = new Integer[]{R.drawable.ic_main_menu_diets, R.drawable.ic_main_menu_reciepes,
+            R.drawable.ic_main_menu_calculating, R.drawable.ic_main_menu_diary, R.drawable.ic_main_menu_newsfeed,
+            R.drawable.ic_main_menu_targets, R.drawable.ic_main_menu_analyzer};
+
+    //private String[] names = new String[] {"Тест", "Тест", "Тест", "Тест", "Тест", "Тест", "Тест"};
+
+    //private String[] names = getResources().getStringArray(R.array.names_items_of_main_screen);
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -101,6 +102,11 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
 
+        rvMainList = findViewById(R.id.rvMainScreen);
+        rvMainList.setLayoutManager(new GridLayoutManager(this, 2));
+        rvMainList.setAdapter(new ItemAdapter(getResources().getStringArray(R.array.names_items_of_main_screen),
+                urlsOfImages, getResources().getStringArray(R.array.properties_items_of_main_screen)));
+
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -117,65 +123,7 @@ public class MainActivity extends AppCompatActivity
         setTitle(getString(R.string.main_menu));
 
 
-        cvMonoDiets = findViewById(R.id.cvMAMonoDiets);
-        cvCalculating = findViewById(R.id.cvMACalculation);
-        cvDiary = findViewById(R.id.cvMADiary);
-        cvRecipes = findViewById(R.id.cvMARecipe);
-        cvAnalyzator = findViewById(R.id.cvMAAnalyzer);
-
-        ivAnalyzator = findViewById(R.id.ivAnalyzerMA);
-        ivCalculating = findViewById(R.id.ivCalculationMA);
-        ivDiary = findViewById(R.id.ivDiaryMA);
-        ivDiets = findViewById(R.id.ivDietsMA);
-        ivRecipes = findViewById(R.id.ivRecipesMA);
-
-        Glide.with(this).load(R.drawable.background_diets).into(ivDiets);
-        Glide.with(this).load(R.drawable.background_analyzer).into(ivAnalyzator);
-        Glide.with(this).load(R.drawable.background_calculate).into(ivCalculating);
-        Glide.with(this).load(R.drawable.background_diary).into(ivDiary);
-        Glide.with(this).load(R.drawable.background_recipes).into(ivRecipes);
-
         loadAd();
-
-
-        //YandexMetrica.reportEvent("Открыта реклама с анимационной кнопки");
-
-
-        cvCalculating.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ActivityListOfCalculating.class);
-                startActivity(intent);
-            }
-        });
-        cvMonoDiets.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ActivityMonoDiet.class);
-                startActivity(intent);
-            }
-        });
-        cvDiary.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ActivityListOfDiary.class);
-                startActivity(intent);
-            }
-        });
-        cvRecipes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ActivityGroupsOfRecipes.class);
-                startActivity(intent);
-            }
-        });
-        cvAnalyzator.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ActivityEmpty.class);
-                startActivity(intent);
-            }
-        });
 
         YandexMetrica.reportEvent("Открыт экран: Стартовый экран");
     }
@@ -246,5 +194,63 @@ public class MainActivity extends AppCompatActivity
             startActivity(intent);
         }
         return true;
+    }
+
+    private class ItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView tvTitle, tvProperties;
+        private ImageView ivImage, ivIsOpen;
+
+        public ItemHolder(LayoutInflater layoutInflater, ViewGroup viewGroup) {
+            super(layoutInflater.inflate(R.layout.item_list_main_menu, viewGroup, false));
+
+            ivImage = itemView.findViewById(R.id.ivMainMenuImage);
+            ivIsOpen = itemView.findViewById(R.id.ivIsOpen);
+            tvTitle = itemView.findViewById(R.id.tvMainMenuTitle);
+            tvProperties = itemView.findViewById(R.id.tvMainMenuProperties);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+
+        public void bind(String name, Integer image, String properties) {
+            tvTitle.setText(name);
+            tvProperties.setText(properties);
+            Glide.with(MainActivity.this).load(image).into(ivImage);
+            if (getAdapterPosition() != 4) {
+                ivIsOpen.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    private class ItemAdapter extends RecyclerView.Adapter<ItemHolder>{
+        String[] names, propeties;
+        Integer[] images;
+
+        public ItemAdapter(String[] names, Integer[] images, String[] propeties) {
+            this.names = names;
+            this.images = images;
+            this.propeties = propeties;
+        }
+
+        @NonNull
+        @Override
+        public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+            return new ItemHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
+            holder.bind(names[position], images[position], propeties[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return names.length;
+        }
     }
 }
--- app/src/main/java/com/wsoteam/diet/TestMainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.wsoteam.diet;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class TestMainActivity extends AppCompatActivity {
-}
