3e1749eb658311d627666a2f40f168c3b07dd9c0
wkazak <wk@wsoteam.com>
2018-12-27 19:29:12 +0300

    cosmetic

--- app/src/main/java/com/wsoteam/diet/BranchOfAnalyzer/ActivityDetailOfFood.java
+++ app/src/main/java/com/wsoteam/diet/BranchOfAnalyzer/ActivityDetailOfFood.java
@@ -5,7 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.github.lzyzsd.circleprogress.DonutProgress;
@@ -13,9 +12,8 @@ import com.wsoteam.diet.POJOFoodItem.ListOfFoodItem;
 import com.wsoteam.diet.R;
 
 public class ActivityDetailOfFood extends AppCompatActivity {
-    private ImageView ivFoodIcon;
-    private TextView tvTitle, tvKcal;
-    private DonutProgress pbCarbohydrates;
+    private TextView tvTitle, tvKcal, tvProtein, tvFat, tvCarbohydrates;
+    private DonutProgress pbCarbohydrates, pbFat, pbProtein;
 
     private ListOfFoodItem foodItem;
 
@@ -25,16 +23,36 @@ public class ActivityDetailOfFood extends AppCompatActivity {
         setContentView(R.layout.activity_detail_of_food);
 
         foodItem = (ListOfFoodItem) getIntent().getSerializableExtra("ActivityDetailOfFood");
-        ivFoodIcon = findViewById(R.id.ivActivityDetailOfFoodCollapsingBackGround);
         tvTitle = findViewById(R.id.tvActivityDetailOfFoodCollapsingTitle);
         tvKcal = findViewById(R.id.tvActivityDetailOfFoodKcal);
+        tvCarbohydrates = findViewById(R.id.tvActivityDetailOfFoodCountOfCarbohydrates);
+        tvFat = findViewById(R.id.tvActivityDetailOfFoodCountOfFat);
+        tvProtein = findViewById(R.id.tvActivityDetailOfFoodCountOfProtein);
         pbCarbohydrates = findViewById(R.id.pbActivityDetailOfFoodCarbo);
+        pbFat = findViewById(R.id.pbActivityDetailOfFoodFat);
+        pbProtein = findViewById(R.id.pbActivityDetailOfFoodProtein);
 
-        Glide.with(this).load(foodItem.getUrlOfImages()).into(ivFoodIcon);
+
         tvTitle.setText(foodItem.getName());
         tvKcal.setText(foodItem.getCalories() + " " + getString(R.string.for_100_g));
-        pbCarbohydrates.setDonut_progress("90");
+        tvFat.setText(getString(R.string.fat_detail) + " " + foodItem.getFat() + "" + getString(R.string.gramm));
+        tvProtein.setText(getString(R.string.protein_detail) + " " + foodItem.getProtein() + " " + getString(R.string.gramm));
+        tvCarbohydrates.setText(getString(R.string.carbohydrates_detail) + " " + foodItem.getCarbohydrates() + " " + getString(R.string.gramm));
+
+        calculateNumbersForProgressBars();
 
 
+    }
+
+    private void calculateNumbersForProgressBars() {
+        Double fat, carbohydrates, protein;
+
+        fat = Double.parseDouble(foodItem.getFat());
+        carbohydrates = Double.parseDouble(foodItem.getCarbohydrates());
+        protein = Double.parseDouble(foodItem.getProtein());
+
+        pbFat.setDonut_progress(String.valueOf(fat.intValue()));
+        pbProtein.setDonut_progress(String.valueOf(protein.intValue()));
+        pbCarbohydrates.setDonut_progress(String.valueOf(carbohydrates.intValue()));
     }
 }
